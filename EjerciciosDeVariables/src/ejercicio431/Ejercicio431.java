204645f010587695a173d51021f57c320c40b43a
Dani <danielporras147@outlook.es>
2023-01-18 20:07:48 +0100

    Ejercicio 431

--- EjerciciosDeVariables/src/ejercicio431/Ejercicio431.java
+++ EjerciciosDeVariables/src/ejercicio431/Ejercicio431.java
@@ -9,22 +9,16 @@ import java.util.Scanner;
 public class Ejercicio431 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
-        System.out.print( "Introduzca el primer número: " );
-        int primerNumero = entrada.nextInt();
-        System.out.print( "Introduzca el segundo número: " );
-        int segundoNumero = entrada.nextInt(); 
-        
-        int resultado = primerNumero * segundoNumero;
-        System.out.println("El resultado de multiplicar " + primerNumero + " X " + segundoNumero + " es: " + resultado);
-        
-        
-        
-		
-		
-				
+
+		System.out.print("Introduzca el primer número: ");
+		int primerNumero = entrada.nextInt();
+		System.out.print("Introduzca el segundo número: ");
+		int segundoNumero = entrada.nextInt();
+
+		int resultado = primerNumero * segundoNumero;
+		System.out.println("El resultado de multiplicar " + primerNumero + " X " + segundoNumero + " es: " + resultado);
 
 	}
 
