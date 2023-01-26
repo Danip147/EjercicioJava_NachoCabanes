2843008086ad94fc56e599f6fa65d8ccfcce848c
Dani <danielporras147@outlook.es>
2023-01-26 14:52:30 +0100

    Ejercicio 615

---
+++ EjerciciosDeBucles/src/ejercicio615/Ejercicio615.java
@@ -0,0 +1,34 @@
+/**
+ * Ejercicio propuesto 6.1.5: Crea un programa que pida al usuario su contraseña (un número entero). 
+ * Se repetirá mientras introduzca una contraseña distinta de "1234".
+ */
+
+package ejercicio615;
+
+import java.util.Scanner;
+
+public class Ejercicio615 {
+	
+	public static void damePassword() {
+		Scanner sc = new Scanner(System.in);
+		int password = 1234;
+		int numUsuario = 0;
+		
+		while(numUsuario != password) {
+			System.out.println("Introduzca contraseña: ");
+			numUsuario = sc.nextInt();
+		}
+		System.out.println("Contraseña correcta!");
+		
+		
+		
+		
+		sc.close();
+		
+	}
+
+	public static void main(String[] args) {
+		damePassword();
+	}
+
+}
