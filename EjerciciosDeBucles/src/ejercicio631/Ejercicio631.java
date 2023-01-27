5a1582f98766f91edf2461cdfa98459356dc40dc
Dani <danielporras147@outlook.es>
2023-01-27 14:48:35 +0100

    Ejercicio 631

---
+++ EjerciciosDeBucles/src/ejercicio631/Ejercicio631.java
@@ -0,0 +1,21 @@
+/**
+ * Ejercicio propuesto 6.3.1: Crea un programa que muestre los números del 1 al 15, usando "for"
+ */
+
+package ejercicio631;
+
+public class Ejercicio631 {
+	
+	public static void muestraNumeros() {
+		
+		for (int i = 1; i <= 15; i++) {
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String[] args) {
+		muestraNumeros();
+
+	}
+
+}
